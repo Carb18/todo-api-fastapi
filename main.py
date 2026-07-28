ee045c5b0f89e2febe34583ebb698311875215fe
Camilo Ruiz <camilofj21@gmail.com>
2026-07-27 21:34:39 -0500

    Stage 1: root and health endpoints

--- main.py
+++ main.py
@@ -4,4 +4,11 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return {"Hello": "Server"}
+    return {"name": "Task API", 
+            "version": "1.0", 
+            "description": "API for managing tasks",
+            "endpoints": ["/tasks"]}
+    
+@app.get("/health")
+async def read_health():
+    return {"status": "ok", "message": "API is healthy and running."}
